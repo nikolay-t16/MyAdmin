e6b38330d5b0327f6e5f05e32d8db98e9c371434
n.tereschenko <n.tereschenko@xPERTS>
2014-09-03 08:45:29 +0400

    Revert "Revert "Init""
    This reverts commit e78a397f5dff1b985e2b28beca73bcad28eeb829.

--- README.md
+++ README.md
@@ -2,3 +2,55 @@ MyAdmin
 =======
 
 My Cms
+
+You must create file "Config.php" in root
+
+<?php
+
+/**
+ * Config of application
+ *
+ * @author Nikolay Tereschenko
+ */
+class Config {
+
+// <editor-fold defaultstate="collapsed" desc="Syte config (domain, url & other)">
+	/**
+	 * Домен
+	 * @var string
+	 */
+	const SITE_DOMAIN_NAME = 'myadmin.ru';
+	const SITE_URL = 'http://myadmin.ru/';
+	const ITEM_TEAMPLATE_PATH = '/component/main/view/admin/item/';
+	const FOLDERS_OF_IMG = "/img/";
+	const FOLDERS_OF_COMPOMEMT = "/component/";
+// </editor-fold>
+// <editor-fold defaultstate="collapsed" desc="Data Base config ( login, password & other )">
+	/**
+	 * Host for db
+	 * @var string
+	 */
+	const DB_HOST = "localhost";
+
+	/**
+	 * Login for db
+	 * @var str
+	 */
+	const DB_LOGIN = "root";
+
+	/**
+	 * Password for db
+	 * @var str
+	 */
+	const DB_PASSWORD = "";
+
+	/**
+	 * название базы
+	 * @var str
+	 */
+	const DB_NAME = "my_admin";
+
+// </editor-fold>
+
+
+}
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include 'Config.php';
+define(ROOT_PATH, __DIR__);
+define(ITEM_TEAMPLATE_PATH, '/component/main/view/admin/item/');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+//$mtime=microtime();
+// <editor-fold defaultstate="collapsed" desc="Автозагрузка классов">
+function ApplicationAutoload($className) {
+	if (!class_exists($className)) {
+		if (class_exist($className))
+			include_once class_exist($className);
+		else{
+			echo '<pre>';
+		echo "$className класс не найден\n";
+		debug_print_backtrace();
+		echo '</pre>';
+		}
+	}
+}
+
+/**
+ * определяет существует ли класс,
+ * @param string $className
+ * @return string
+ * возвращает путь к классу если он существует, если нет возвращает 0
+ */
+function class_exist($className) {
+	$class_path = glob(ROOT_PATH . Config::FOLDERS_OF_COMPOMEMT . '*/*/' . $className . '.php');
+	if ($class_path) {
+		return $class_path[0];
+	} else {
+		return 0;
+	}
+}
+
+spl_autoload_register('ApplicationAutoload');
+// </editor-fold>
+echo app::I()->IndexRun();
+//echo microtime()-$mtime;
