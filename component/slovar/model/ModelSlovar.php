45f37b8c11a1dd68a9fee0eca76ca8a10a2964a3
n.tereschenko <nikolay.ter@gmail.com>
2015-05-06 11:31:44 +0400

    Добавлены методы GetSloavrItems и GetSlovarVal

--- component/slovar/model/ModelSlovar.php
+++ component/slovar/model/ModelSlovar.php
@@ -10,6 +10,23 @@
  * @author Терещенко
  */
 class ModelSlovar extends model{
-    
+	/**
+	 * Возращает все значения словаря
+	 * @param int $idSlovar
+	 * @return array
+	 */
+	public function GetSlovarItems($idSlovar){
+		return $this->GetItems("p_id=$idSlovar order by name ASC");
+	}
+
+	/**
+	 * Возращает имя элемента
+	 * @param int $idItem
+	 * @return string
+	 */
+	public function GetSloavrVal($idItem) {
+		$item = $this->GetItem($idItem);
+		return $item?$item['name']:NULL;
+	}
 }
 ?>
