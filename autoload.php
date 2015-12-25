e036f47da9971e565304e5fa36e8659bc6105c8c
n.tereschenko <nikolay.ter@gmail.com>
2015-12-25 11:29:50 +0300

    Функции автозагрузки классов вынесены в отдельный файл

---
+++ autoload.php
@@ -0,0 +1,33 @@
+<?php
+
+function ApplicationAutoload($className) {
+	if (!class_exists($className)) {
+		$class_path = class_exist($className);
+
+		if ($class_path) {
+			include_once class_exist($className);
+		} else {
+			echo '<pre>';
+			echo "$className класс не найден\n";
+			debug_print_backtrace();
+			echo '</pre>';
+		}
+	}
+}
+
+/**
+ * определяет существует ли класс,
+ * @param string $className
+ * @return string
+ * возвращает путь к классу если он существует, если нет возвращает 0
+ */
+function class_exist($className) {
+	$class_path = glob(ROOT_PATH . Config::FOLDERS_OF_COMPOMEMT . '*/*/' . $className . '.php');
+	if ($class_path) {
+		return $class_path[0];
+	} else {
+		return 0;
+	}
+}
+
+spl_autoload_register('ApplicationAutoload');
